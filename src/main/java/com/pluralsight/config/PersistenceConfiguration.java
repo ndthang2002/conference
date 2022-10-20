6bed199eb4f00d6ee2a252ad13ce92e124de485b
Thắng Nguyễn Đình <95957411+nguyendinhthangdeptrai2002@users.noreply.github.com>
2022-10-20 11:46:22 +0700

    delete config

--- src/main/java/com/pluralsight/config/PersistenceConfiguration.java
+++ src/main/java/com/pluralsight/config/PersistenceConfiguration.java
@@ -9,12 +9,12 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class PersistenceConfiguration {
-  @Bean
-  public DataSource dataSource() {
-  DataSourceBuilder builder = DataSourceBuilder.create();
-  builder.url("jdbc:postgresql://localhost:5432/conference_app");
-  System.out.println("My custom datasource bean has been initialize and set");
-    return builder.build();
-    
-  }
+//  @Bean
+//  public DataSource dataSource() {
+//  DataSourceBuilder builder = DataSourceBuilder.create();
+//  builder.url("jdbc:postgresql://localhost:5432/conference_app");
+//  System.out.println("My custom datasource bean has been initialize and set");
+//    return builder.build();
+//    
+//  }
 }
